0109ecbf8037cf5b732680d0564d9255b2bcf2d3
nikolaivasilev12 <nikolaivasilev208@gmail.com>
2021-03-22 12:27:47 +0100

    heroku attempt #1

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.API_PORT || 8080;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
